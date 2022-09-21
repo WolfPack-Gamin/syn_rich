bdf5dc5f0a0238010ad52bab7f9b4215fc8e1e31
JusCampin <37031311+JusCampin@users.noreply.github.com>
2022-09-20 20:56:10 -0700

    Initial commit

---
+++ client.lua
@@ -0,0 +1,45 @@
+-- Rich Presence Discord for RedM
+-- read the readme.txt for instructions
+local playercount
+Citizen.CreateThread(function()
+	while true do
+       
+		SetDiscordAppId(Config.appid)
+
+        
+		SetDiscordRichPresenceAsset(Config.biglogo)
+
+
+        SetDiscordRichPresenceAssetText(Config.biglogodesc)
+       
+        
+        SetDiscordRichPresenceAssetSmall(Config.smalllogo)
+
+        -- hover text for the "small" icon.(OPTIONAL)
+        SetDiscordRichPresenceAssetSmallText(Config.smalllogodesc)
+
+        SetDiscordRichPresenceAction(0, "Join Discord", Config.discordlink) -- copy and paste if u want new buttons, change 0 to the next number, so the next button would be 1 then 2 etc
+        
+        TriggerServerEvent("syn_rich:getplayers")
+        while playercount == nil do 
+            Wait(500)
+        end
+        if Config.shownameandid then
+            local pId = GetPlayerServerId(PlayerId())
+            local pName = GetPlayerName(PlayerId())    
+            SetRichPresence(playercount.."/"..Config.maxplayers.." - ID: "..pId.." | "..pName)
+        else
+            SetRichPresence(playercount.."/"..Config.maxplayers)
+        end
+		Citizen.Wait(60000) -- 1 min update
+        playercount = nil
+	end
+end)
+
+
+RegisterNetEvent("syn_rich:update")
+AddEventHandler("syn_rich:update", function(x)
+	playercount = x
+end)
+
+
---
+++ config.lua
@@ -0,0 +1,11 @@
+-- how to set up: https://forum.cfx.re/t/how-to-updated-discord-rich-presence-custom-image/157686
+Config = {}
+Config.maxplayers = 64
+Config.appid =  -- Application ID (Replace this with you own)
+Config.biglogo = "" -- image assets name for the "large" icon.
+Config.biglogodesc = " " -- text when hover over image 
+Config.smalllogo = ""  -- image assets name for the "small" icon.(OPTIONAL)
+Config.smalllogodesc = "" -- text when hover over image 
+Config.discordlink = "" -- discord link 
+Config.shownameandid = true --show player steam name and id 
+
